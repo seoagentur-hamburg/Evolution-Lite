a84629876464ea49b3135736e1ede4948df237e0
Andreas Hecht <andreas.hecht@hechtmediaarts.com>
2017-11-07 16:19:15 +0100

    Kleine Änderungen

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
  *
  * @package Evolution Framework
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
--- page.php
+++ page.php
@@ -10,22 +10,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 		
-		<?php do_action( 'evolution_before_loop' ); ?>
+		<?php do_action( 'evolution_before_loop' );
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
+            do_action( 'evolution_page' );
 		
-		<?php do_action( 'evolution_after_loop' ); ?>
+		    do_action( 'evolution_after_loop' ); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
